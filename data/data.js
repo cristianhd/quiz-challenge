255a33fd4b8fffeabbbb74d8e2ba7b339efa4a71
cristianhd <cristian.hd1996@gmail.com>
2021-10-16 16:46:15 -0300

    create list of questions

---
+++ data/data.js
@@ -0,0 +1,152 @@
+export const data = [
+    {
+        question: "¿En qué continente está España?",
+        level: "1",
+        options:["América", "Europa", "Asia", "África"],
+        answer: "Europa"        
+    },
+    {
+        question: "¿Quién es el autor de el Quijote?",
+        level: "1",
+        options:["Miguel Cervantes", "Homero", "Gabriel Garcia Marquez", "Jorge Luis Borges"],
+        answer: "Miguel Cervantes"        
+    },
+    {
+        question: "¿En qué país se encuentra la torre de Pisa?",
+        level: "1",
+        options:["Francia", "Londres", "España", "Italia"],
+        answer: "Italia"        
+    },
+    {
+        question: "¿Qué deporte practicaba Michael Jordan?",
+        level: "1",
+        options:["Fútbol", "Baloncesto", "Voleibol", "Tenis"],
+        answer: "Baloncesto"        
+    },
+    {
+        question: "¿Cuál es país más poblado de la Tierra?",
+        level: "1",
+        options:["China", "Rusia", "Colombia", "Brazil"],
+        answer: "China"        
+    },
+    {
+        question: "¿Cuántas patas tiene la araña?",
+        level: "2",
+        options:["4", "6", "10","8"],
+        answer: "8"        
+    },
+    {
+        question: "¿Quién era el general de los nazis en la Segunda Guerra Mundial?",
+        level: "2",
+        options:["Adolf Hitler", "Napoleón Bonaparte", "Josef Stalin", "Kim Sung"],
+        answer: "Adolf Hitler"        
+    },
+    {
+        question: "¿Cuál es la moneda del Reino Unido?",
+        level: "2",
+        options:["Dolar", "Peso", "Libra", "Yuan"],
+        answer: "Libra"        
+    },
+    {
+        question: "¿Quién escribió La Odisea?",
+        level: "2",
+        options:["Leonardo Di Caprio", "Homero", "Miguel Cervantes", "Carles Dickens"],
+        answer: "Homero"        
+    },
+    {
+        question: "¿Cómo se llama la Reina del Reino Unido?",
+        level: "2",
+        options:["Ana", "Maria", "Isabella", "Isabel"],
+        answer: "Isabel"        
+    },
+    {
+        question: "¿Qué tipo de animal es la ballena?",
+        level: "3",
+        options:["Pez", "Reptil", "Mamifero", "Anfibio"],
+        answer: "Mamifero"        
+    },
+    {
+        question: "¿Qué cantidad de huesos tiene un adulto?",
+        level: "3",
+        options:["120", "202", "300", "152"],
+        answer: "300"        
+    },
+    {
+        question: "¿Cuándo acabó la II Guerra Mundial?",
+        level: "3",
+        options:["1945", "1495", "1480", "1923"],
+        answer: "1945"        
+    },
+    {
+        question: " ¿Cuál es el océano más grande?",
+        level: "3",
+        options:["Índico", "Pacífico", "Antártico", "Atlántico"],
+        answer: ""        
+    },
+    {
+        question: "¿Quién es el famoso Rey de Rock en los Estados Unidos?",
+        level: "3",
+        options:["Michael Jackson", "Elvis Presley", "Jim Morrison", "Albert King"],
+        answer: "Elvis Presley"        
+    },
+    {
+        question: "¿Cuál es la capital de Croacia?",
+        level: "4",
+        options:["Zagreb", "Kissing", "Drama", "Ámsterdam"],
+        answer: "Zagreb"        
+    },
+    {
+        question: "¿En qué país se usó la primera bomba atómica en combate?",
+        level: "4",
+        options:["Estados Unidos", "China", "Holanda", "Japón"],
+        answer: "Japón"        
+    },
+    {
+        question: "¿Cuál es el país con más camellos salvajes?",
+        level: "4",
+        options:["Holanda", "Egipto", "Australia", "Canadá"],
+        answer: "Australia"        
+    },
+    {
+        question: "¿De qué país es originario el café?",
+        level: "4",
+        options:["Colombía","Etipía","Brazil","Bolivia"],
+        answer: "Etipía"        
+    },
+    {
+        question: "¿Dónde se celebraron los Juegos Olímpicos del año 1992?",
+        level: "4",
+        options:["Berlin", "Tokio", "Barcelona", "Grecia"],
+        answer: "Barcelona"        
+    },
+    {
+        question: " ¿En qué año viajó al espacio el primer ser humano?",
+        level: "5",
+        options:["1940","1961","1915","1890"],
+        answer: "1961"        
+    },
+    {
+        question: " ¿Qué instrumento tocaba Paco de Lucía?",
+        level: "5",
+        options:["Guitarra","Piano","Violín","Flauta"],
+        answer: "Guitarra"        
+    },
+    {
+        question: "¿Quién dirigió “El Lobo de Wall Street”?",
+        level: "5",
+        options:["Martin Scorsese","Steven Spielberg", "James Cameron", "Michael Bay"],
+        answer: "Martin Scorsese"        
+    },
+    {
+        question: "¿Cómo se llama el primer país en colonizar el territorio de lo que hoy se conoce como Alaska?",
+        level: "5",
+        options:["Reino Unido", "Alemanía", "Rusia", "Canadá"],
+        answer: "Rusia"        
+    },
+    {
+        question: "¿Qué elemento químico fue nombrado en honor a un malvado duende del subsuelo?",
+        level: "5",
+        options:["Hafnio","Cobalto", "Berilio", "Telurio"],
+        answer: "Cobalto"        
+    }
+]
